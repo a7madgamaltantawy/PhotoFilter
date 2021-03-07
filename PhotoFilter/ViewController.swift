a725fc1902f559041ce751e3f16896791c9edbc9
ahmed tantawy <a7mad_gamal@live.com>
2021-03-08 00:10:27 +0200

    sepia filter

--- PhotoFilter/ViewController.swift
+++ PhotoFilter/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+   
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    // property to hold the selected picture
+    
+    var currentImage : UIImage!
+    
+    var context : CIContext!
+    var currentFilter : CIFilter!
+
+    
+    
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "YACIFP"
+        
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem (barButtonSystemItem:.add, target: self, action: #selector(importPicture))
+        
+        
+        
+        context = CIContext()
+        
+        currentFilter = CIFilter(name: "CISepiaTone")
+        
+        
+        
+        
+        
+        
     }
 
 
+    @objc func importPicture(){
+        
+        let picker = UIImagePickerController()
+        
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker , animated: true)
+        
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return }
+        
+        dismiss(animated: true)
+        currentImage = image
+    }
+    
+    
 }
 
